b44b47ee0b97d30c6c69ded1a244b7865bfc21d6
Brian <briankgathui@gmail.com>
2025-02-06 08:19:53 +0300

    Improved 404 page

--- client/src/pages/notfound.js
+++ client/src/pages/notfound.js
@@ -6,7 +6,7 @@ const NotFound = () => {
         <div className="dashboard-container">
             <div className="d-flex justify-content-center align-items-center" style={{ height: "100vh" }}>
                 <div className="col-md-12 text-center">
-                    <h1>404</h1>
+                    <h1>🚫 404 🚫</h1>
                     <h2>Page Not Found</h2>
                     <p>Sorry, the page you are looking for does not exist.</p>
                 </div>
@@ -15,4 +15,4 @@ const NotFound = () => {
     );
 };
 
-export default NotFound;
+export default NotFound;``
