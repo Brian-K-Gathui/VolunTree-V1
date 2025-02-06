c2d565c6c95551e667c27ef580442612de908f30
Brian <briankgathui@gmail.com>
2025-02-06 08:53:08 +0300

    OrganizationResource and OrganizationByIdResource import correction in __init__.py

--- server/routes/__init__.py
+++ server/routes/__init__.py
@@ -1,13 +1,13 @@
 from flask_restful import Api
 from server.routes.admin_routes import AdminSignup, AdminLogin, AdminProfile
 from server.routes.event_routes import EventResource, EventByIdResource
-from server.routes.organization_routes import OrganizerResource, OrganizerByIdResource
+from server.routes.organization_routes import OrganizationResource, OrganizationByIdResource
 from server.routes.volunteer_routes import VolunteerResource, VolunteerByIdResource
 from server.routes.task_routes import TaskResource, TaskByIdResource
 
 def register_routes(api: Api):
-    api.add_resource(OrganizerResource, '/api/organizations')
-    api.add_resource(OrganizerByIdResource, '/api/organizations/<int:organizer_id>')
+    api.add_resource(OrganizationResource, '/api/organizations')
+    api.add_resource(OrganizationByIdResource, '/api/organizations/<int:organizer_id>')
 
     api.add_resource(EventResource, '/api/events')
     api.add_resource(EventByIdResource, '/api/events/<int:event_id>')
